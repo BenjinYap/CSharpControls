aa72a6187bad0635046f68cba4a088b0fadefdc8
Benjin Yap <benjyay.dev@gmail.com>
2014-05-15 11:36:40 -0400

    preparation for dockable form class.

---
+++ DockManager/CSSDockableForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpControls.DockManager {
+	public class CSSDockableForm:Form {
+		internal new string Name = String.Empty;
+		internal bool Docked = false;
+		internal TabPage TabPage = null;
+		internal TabControl TabControl = null;
+		internal int TabIndex = -1;
+	}
+}
